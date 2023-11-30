ce5297009449f984b84ee1f5f9d7d179276eea48
kepya <christiankepya@gmail.com>
2023-11-30 02:30:38 +0100

    first commit

---
+++ src/test/java/com/selenium/test/projets/cimencam/NewOrderTestFeature.java
@@ -0,0 +1,19 @@
+package com.selenium.test.projets.cimencam;
+
+import com.selenium.test.junit.rules.ScreenShotOnFailRule;
+import com.selenium.test.webtestsbase.WebDriverFactory;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.log4testng.Logger;
+
+/**
+ * Created by KEPYA Christian on 30.11.23.
+ * Uses JUnit test framework
+ * Class for testing new order feature (client and admin login)
+ */
+public class NewOrderTestFeature {
+}
